3e90c04fb9dec928d27fb904a8f15e7d0fe1cf49
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-06-02 14:02:26 +0100

    Modify Calculator.js & App.js  file for refactoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './index.css';
 import React from 'react';
-import Design from './components/Calculator';
+import Calculator from './components/Calculator';
 
 function App() {
   const buttonNames = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-  
+  const data = {
+    total: '',
+    operation: '',
+    next: '0'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Design data={buttonNames} />
+        <Calculator data={buttonNames} obj={data} />
       </header>
     </div>
   );
