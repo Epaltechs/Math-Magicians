bfecbc8726d7a438e2c2aaa03ccd63610bb8fd88
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-06-07 15:58:22 +0100

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './index.css';
 import React from 'react';
+import Design from './components/Calculator';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Quote from './components/Quote';
-import Calculator from './components/Calculator';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const buttonNames = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
@@ -15,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Calculator data={buttonNames} obj={data} />
+        <Navbar />
         <Routes>
           <Route exact path='/design' element={<Design data={buttonNames} obj={data} />} />
           <Route path='/' element={<Home />} />
