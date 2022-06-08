dde060c68359c5c20d1b08f99f29d5407f2952f2
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-06-08 10:00:43 +0100

    Edit Navbar.css file

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ const Home = () => {
     return (
         <div className="home-link-div">
             <h2 className="header-text">Welcome to our page</h2>
-            <p className="welcome-paragraph">It has been said that an Amateur is defensive.
-            The professional finds learning-and even occassionally being shown up-to be enjoyable;
+            <p className="welcome-paragraph">It has been said that an amateur is defensive.
+            The professional finds learning - and even occassionally being shown up - to be enjoyable;
             they like being challenged and humbled, and engage in education as an ongoing and endless process.
             Stay humble because the second you stop being a student, your knowledge becomes fragile.
             I might even add that the moment you stop learning, you start dying.
